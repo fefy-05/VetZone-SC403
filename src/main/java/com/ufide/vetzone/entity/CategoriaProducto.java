209a9014e9898fe433c7b7c98538163103366b18
fefy-05 <stephachav@gmail.com>
2026-07-14 12:43:17 -0600

    Completa entidad CategoriaProducto

--- src/main/java/com/ufide/vetzone/entity/CategoriaProducto.java
+++ src/main/java/com/ufide/vetzone/entity/CategoriaProducto.java
@@ -1,5 +1,39 @@
 package com.ufide.vetzone.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categorias_productos")
+
 public class CategoriaProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 50)
+    @Column(nullable = false, unique = true, length = 50)
+    private String nombre;
+
+    public CategoriaProducto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
 }
